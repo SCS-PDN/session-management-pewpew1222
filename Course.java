037b369446e947474661375809d976c1677cdf7f
sarah <sarathijayaekara@gmail.com>
2025-05-02 17:53:50 +0530

    Completed Course Registration System

---
+++ Course.java
@@ -0,0 +1,27 @@
+package com.studentportal;
+public class Course {
+    private String courseId;
+    private String courseName;
+    private String instructor;
+
+    public Course(String courseId, String courseName, String instructor) {
+        this.courseId = courseId ;
+        this.courseName  = courseName;
+        this.instructor = instructor;
+    }
+
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+
+    public String getInstructor() {
+        return instructor;
+    }
+}
